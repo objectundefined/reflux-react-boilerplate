a40e9184e49b76ce8389ae8908bad3aaf7b60f32
objectundefined <gabriel.lipson@gmail.com>
2016-01-12 15:50:23 -0800

    correctly match promise and evt

--- app/js/stores/CommentsStore.js
+++ app/js/stores/CommentsStore.js
@@ -3,20 +3,25 @@ import * as actions from '../actions'
 
 export default Reflux.createStore({
   comments: [{ user: {name: 'Gabe Lipson'}, id: Date.now(), text:'Hello world' }],
-  listenables: [ actions.comments ],
+  // listenables: [ actions.comments ],
+  init: function(){
+    actions.comments.add.listenAndPromise(this.add, this)
+  },
   getInitialState: function() {
       return this.comments;
   },
   add: function (c) {
-    // set a temp id
-    c.id = Date.now();
-    // trigger update with pending data, wait for server to reconcile.
-    this.comments.push(c)
-    this.trigger(this.comments)		
-    setTimeout(()=>{
-      c.pending = false;
-			this.trigger(this.comments)
-			actions.comments.add.completed(c);
-    },1000)
+      // set a temp id
+      c.id = Date.now();
+      // trigger update with pending data, wait for server to reconcile.
+      this.comments.push(c)
+      this.trigger(this.comments)
+      return new Promise((resolve)=>{
+        setTimeout(()=>{
+          c.pending = false;
+          this.trigger(this.comments)
+          resolve(c)
+        },5000)
+      })
   }
 });
